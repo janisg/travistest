fc9f112163d165f4d9be6d12de739fc3e0739190
Janis Gailis <tellme@jaanis.org>
2015-10-13 10:37:20 +0200

    fixed the semi-colon error in hello

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello TravisCI\n') // missing a semi-colon with fail to build
+  res.end('Hello TravisCI\n'); // missing a semi-colon with fail to build
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
